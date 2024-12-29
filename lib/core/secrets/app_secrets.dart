c698770d9683cc485250f315d3fd552b3c66e698
AanishRahmani <rahmaniaanish@gmail.com>
2024-12-30 00:01:10 +0530

    secrets safe

--- lib/core/secrets/app_secrets.dart
+++
@@ -1,5 +0,0 @@
-class AppSecrets {
-  static const supabaseUrl = 'https://owpcexrgjfbvzuxkcctd.supabase.co';
-  static const supabaseAnonKey =
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im93cGNleHJnamZidnp1eGtjY3RkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzU0MTAxNDMsImV4cCI6MjA1MDk4NjE0M30.ZTRHFs9z65n6NYXWay7QcL8WLlxMz0m9Srzo7_P-xzU';
-}
